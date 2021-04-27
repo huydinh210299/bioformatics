db67d451229296c3dc5c86ea62a2996abbd99889
huydinh210299 <dinhlun99hy@gmail.com>
2021-04-27 09:57:54 +0700

    -push backend code

---
+++ Backend/ML/classify.py
@@ -0,0 +1,69 @@
+## import thu vien
+import os
+from Pfeature.pfeature import paac_wp
+import pandas as pd
+import pickle
+from Bio import SeqIO
+import random
+import string
+import threading
+
+
+class Classify:
+
+  # Ham thuc hien generate du lieu
+  def paac(seft, input):
+    a = input.rstrip('fasta')
+    output = a + 'paac.csv'
+    df_out = paac_wp(input, output,4,0.2)
+    df = pd.read_csv(output)
+
+  # Ham luu seq truyen vao thanh file va genarate ra du lieu
+  def paacBySeq(seft, seq):
+    fileName = string.ascii_lowercase
+    fileName = ''.join(random.choice(fileName) for i in range(10))
+    fileName = fileName + ".fasta"
+    seft.filename = fileName
+    with open(fileName, "w") as file:
+      # Writing data to a file
+      file.write(">001\n")
+      file.write(seq)
+    genarateThread = threading.Thread(target=seft.paac,args=(fileName,))
+    genarateThread.start()
+    return fileName
+
+  def paacByFile(seft, input_fname):
+    fileName = string.ascii_lowercase
+    fileName = ''.join(random.choice(fileName) for i in range(10))
+    fileName = fileName + ".fasta"
+    os.rename(input_fname, fileName)
+    genarateThread = threading.Thread(target=seft.paac,args=(fileName,))
+    genarateThread.start()
+    return fileName
+
+  # Ham load model
+  def loadModel(seft):
+    with open('./model/model.pkl', 'rb') as file:
+      pickle_model = pickle.load(file)
+    return pickle_model 
+
+  # Ham chuyen tu file fasta thanh array
+  def fasta_to_Array(seft, input):
+    arr = []
+    for record in SeqIO.parse(input, "fasta"):
+      item = [record.id,str(record.seq)]
+      arr.append(item)
+    return arr
+
+  # Ham thuc hien phan loai voi file
+  def antiMicroable(seft, input_fname):
+    df = pd.read_csv(input_fname)
+    model = seft.loadModel()
+    yPred = model.predict(df)
+    yPred = pd.DataFrame(yPred,columns=['class'])
+    fasta_file = input_fname.rstrip('.paac.csv') + '.fasta'
+    mydict = seft.fasta_to_Array(fasta_file)
+    df1 = pd.DataFrame(mydict,columns=['ID', 'Seq'])
+    result = pd.concat([df1, yPred], axis=1, ignore_index=True)
+    result.columns = ["ID", "Seq", "Class"]
+    return result
---
+++ Backend/api.py
@@ -0,0 +1,79 @@
+from flask import Flask, request
+from flask_restplus import Resource, Api, reqparse, fields, marshal
+from flask_cors import CORS
+from ML.classify import Classify
+from werkzeug.exceptions import BadRequest
+from werkzeug.datastructures import FileStorage
+import json
+import os
+
+
+app = Flask(__name__)
+CORS(app)
+api = Api(app)
+
+classification = Classify()
+
+@api.route('/hello')
+class HelloWorld(Resource):
+    def get(Self):
+        return {'hello': 'world'}
+
+### /result?filename=xxx
+@api.route('/result')
+class Classify(Resource):
+    def get(Self):
+        try:
+            args = request.args
+            filename = args["filename"]
+            rs =  classification.antiMicroable('./'+ filename)
+            result = rs.to_json(orient="records")
+            parsed = json.loads(result)
+            json.dumps(parsed, indent=4)
+            if os.path.exists(filename):
+                os.remove(filename)
+            filename = filename.rstrip('.paac.csv') + '.fasta'
+            if os.path.exists(filename):
+                os.remove(filename)
+            return parsed
+        except:
+            raise BadRequest()
+
+### Thuc hien upload input la sequence
+parser = reqparse.RequestParser()
+parser.add_argument('seq', type=str)
+
+@api.route('/upload-data/seq')
+@api.expect(parser)
+class UploadSeq(Resource):
+    def post(Self):
+        try:
+            args = parser.parse_args()
+            proseq = args['seq']
+            fn_fasta = classification.paacBySeq(proseq)
+            fn_csv = fn_fasta.strip('.fasta') + '.paac.csv'
+            return {'filename': fn_csv}
+        except:
+            raise BadRequest()
+
+### Thuc hien upload la file fasta
+upload_parser = api.parser()
+upload_parser.add_argument('file', location='files',
+                           type=FileStorage, required=True)
+
+@api.route('/upload-data/fastafile')
+@api.expect(upload_parser)
+class Upload(Resource):
+    def post(self):
+        try:
+            args = upload_parser.parse_args()
+            uploaded_file = args['file']
+            fn = uploaded_file.filename
+            uploaded_file.save(fn)
+            fn_fasta = classification.paacByFile(fn)
+            fn_csv = fn_fasta.strip('.fasta') + '.paac.csv'
+            return {'filename': fn_csv}
+        except:
+            raise BadRequest()
+if __name__ == '__main__':
+    app.run(debug=True)
